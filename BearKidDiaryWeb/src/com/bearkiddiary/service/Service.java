1d10dedf2b6a599248b09a4d79d2fe005fab7528
hxuuaanng@qq.com <hxuuaanng@qq.com>
2016-07-25 17:23:51 +0800

    简单的测试

--- BearKidDiaryWeb/src/com/bearkiddiary/service/Service.java
+++ BearKidDiaryWeb/src/com/bearkiddiary/service/Service.java
@@ -1,5 +1,15 @@
 package com.bearkiddiary.service;
 
+import com.bearkiddiary.bean.User;
+
 public interface Service {
+	
+	/**
+	 * 测试
+	 * @param user 获取的用户
+	 * @return
+	 */
+	//登录功能
+	boolean Login(User user);
 
 }
--- BearKidDiaryWeb/src/com/bearkiddiary/service/impl/ServiceImpl.java
+++ BearKidDiaryWeb/src/com/bearkiddiary/service/impl/ServiceImpl.java
@@ -1,7 +1,29 @@
 package com.bearkiddiary.service.impl;
 
+import java.util.List;
+
+import com.bearkiddiary.bean.User;
+import com.bearkiddiary.dao.TestDao;
 import com.bearkiddiary.service.Service;
 
 public class ServiceImpl implements Service{
+
+	/**
+	 * ≤‚ ‘
+	 */
+	private TestDao testDao;
+	
+	public void setTestDao(TestDao testDao) {
+		this.testDao = testDao;
+	}
+	@Override
+	public boolean Login(User user) {
+		// TODO Auto-generated method stub
+		List<User> userList = testDao.Login(user);
+		if(!userList.isEmpty()){
+			return true;
+		}
+		return false;
+	}
 
 }
